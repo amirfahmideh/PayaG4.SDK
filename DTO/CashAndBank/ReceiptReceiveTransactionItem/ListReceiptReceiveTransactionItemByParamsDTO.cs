9a9f7a9e7792f90a1f93a11185d734d4943414ae
Amir Fahmideh <amir.fahmideh@gmail.com>
2024-11-20 14:38:00 +0330

    add bankDocumentCode

--- DTO/CashAndBank/ReceiptReceiveTransactionItem/ListReceiptReceiveTransactionItemByParamsDTO.cs
+++ DTO/CashAndBank/ReceiptReceiveTransactionItem/ListReceiptReceiveTransactionItemByParamsDTO.cs
@@ -27,6 +27,10 @@ namespace PayaG4.SDK.DTO.CashAndBank
         [Required(ErrorMessage = "َشماره سند را وارد کنید")]
         public string DocNo { get; set; }
 
+        [Display(Name = "نوع سند")]
+        [Required(ErrorMessage = "نوع سند را وارد کنید")]
+        public int? BankDocumentCode { get; set; }
+
         [Display(Name = "سال مالی")]
         public int? FYearId { get; set; }
 
