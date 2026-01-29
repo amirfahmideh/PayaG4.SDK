3d69b4b264c67db94fd22a2ea7e50a85288161b0
Abdollahi <kiyana1359@gmail.com>
2026-01-29 10:10:49 +0330

    Call Services Of Update ExternalReferenceId

--- CashAndBank/V1/TrsTransaction/GeneralTrsTransactionService.cs
+++ CashAndBank/V1/TrsTransaction/GeneralTrsTransactionService.cs
@@ -1,15 +1,17 @@
-using System.Net;
-using System.Net.Http.Json;
 using PayaG4.SDK.Base;
 using PayaG4.SDK.DTO.CashAndBank;
 using PayaG4.SDK.DTO.General.MethodResults;
 using PayaG4.SDK.Exceptions;
+using System.Net;
+using System.Net.Http.Json;
 
 namespace PayaG4.SDK.CashAndBank.V1.TrsTransaction;
 public class GeneralTrsTransactionService : BaseService
 {
     private HttpClient httpClient;
     private readonly string apiPrefix = "/api/v1/trsTransaction/";
+    private readonly string apiCashPrefix = "/api/v1/Cash/";
+    private readonly string apiDocumentsPrefix = "/api/v1/Documents/";
     public GeneralTrsTransactionService(ServiceConfiguration _serviceConfiguration) : base(_serviceConfiguration)
     {
         httpClient = new HttpClient();
@@ -70,6 +72,142 @@ public class GeneralTrsTransactionService : BaseService
         catch (Exception ex)
         {
             return MethodResult<ReceiptReceiveTransactionItemDTO>.CloneSimpleErrorMethodResult("خطا", ex.Message);
+        }
+    }
+
+    /******************************************* C A S H         S E R V I C E S ********************************************************************************************************************/
+
+    public async Task<MethodResult<bool>> UpdateExternalReferenceIdGetCashItem(UpdateExternalReferenceIdTransactionItemDTO parameter)
+    {
+        try
+        {
+            await AddAuthorizationBearerAsync(httpClient);
+            var clientUrl = GenerateApiCallUrl(apiCashPrefix, "updateExternalReferenceIdGetCashItem");
+            var response = await httpClient.PostAsJsonAsync(clientUrl, parameter);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseResult = await response.Content.ReadFromJsonAsync<MethodResult<bool>>();
+                if (responseResult != null)
+                    return responseResult;
+                else { return MethodResult<bool>.CloneSimpleErrorMethodResult("خطا", "خطا در دسترسی به وب سرویس"); }
+            }
+            else if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                throw new UnauthorizedException();
+            }
+            else if (response.StatusCode == HttpStatusCode.BadRequest)
+            {
+                throw new BadRequestException();
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+        catch (Exception ex)
+        {
+            return MethodResult<bool>.CloneSimpleErrorMethodResult("خطا", ex.Message);
+        }
+    }
+
+    public async Task<MethodResult<bool>> UpdateExternalReferenceIdPayCashItem(UpdateExternalReferenceIdTransactionItemDTO parameter)
+    {
+        try
+        {
+            await AddAuthorizationBearerAsync(httpClient);
+            var clientUrl = GenerateApiCallUrl(apiCashPrefix, "updateExternalReferenceIdPayCashItem");
+            var response = await httpClient.PostAsJsonAsync(clientUrl, parameter);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseResult = await response.Content.ReadFromJsonAsync<MethodResult<bool>>();
+                if (responseResult != null)
+                    return responseResult;
+                else { return MethodResult<bool>.CloneSimpleErrorMethodResult("خطا", "خطا در دسترسی به وب سرویس"); }
+            }
+            else if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                throw new UnauthorizedException();
+            }
+            else if (response.StatusCode == HttpStatusCode.BadRequest)
+            {
+                throw new BadRequestException();
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+        catch (Exception ex)
+        {
+            return MethodResult<bool>.CloneSimpleErrorMethodResult("خطا", ex.Message);
+        }
+    }
+
+    /******************************************* D O C U M E N T S         S E R V I C E S ********************************************************************************************************************/
+
+    public async Task<MethodResult<bool>> UpdateExternalReferenceIdReceiveDocumentsItem(UpdateExternalReferenceIdTransactionItemDTO parameter)
+    {
+        try
+        {
+            await AddAuthorizationBearerAsync(httpClient);
+            var clientUrl = GenerateApiCallUrl(apiDocumentsPrefix, "updateExternalReferenceIdReceiveDocumentsItem");
+            var response = await httpClient.PostAsJsonAsync(clientUrl, parameter);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseResult = await response.Content.ReadFromJsonAsync<MethodResult<bool>>();
+                if (responseResult != null)
+                    return responseResult;
+                else { return MethodResult<bool>.CloneSimpleErrorMethodResult("خطا", "خطا در دسترسی به وب سرویس"); }
+            }
+            else if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                throw new UnauthorizedException();
+            }
+            else if (response.StatusCode == HttpStatusCode.BadRequest)
+            {
+                throw new BadRequestException();
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+        catch (Exception ex)
+        {
+            return MethodResult<bool>.CloneSimpleErrorMethodResult("خطا", ex.Message);
+        }
+    }
+
+    public async Task<MethodResult<bool>> UpdateExternalReferenceIdPayDocumentsItem(UpdateExternalReferenceIdTransactionItemDTO parameter)
+    {
+        try
+        {
+            await AddAuthorizationBearerAsync(httpClient);
+            var clientUrl = GenerateApiCallUrl(apiDocumentsPrefix, "updateExternalReferenceIdPayDocumentsItem");
+            var response = await httpClient.PostAsJsonAsync(clientUrl, parameter);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseResult = await response.Content.ReadFromJsonAsync<MethodResult<bool>>();
+                if (responseResult != null)
+                    return responseResult;
+                else { return MethodResult<bool>.CloneSimpleErrorMethodResult("خطا", "خطا در دسترسی به وب سرویس"); }
+            }
+            else if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                throw new UnauthorizedException();
+            }
+            else if (response.StatusCode == HttpStatusCode.BadRequest)
+            {
+                throw new BadRequestException();
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+        catch (Exception ex)
+        {
+            return MethodResult<bool>.CloneSimpleErrorMethodResult("خطا", ex.Message);
         }
     }
 }
---
+++ DTO/CashAndBank/ExternalReferenceIdTransactionItem/UpdateExternalReferenceIdTransactionItemDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PayaG4.SDK.DTO.CashAndBank
+{
+    public class UpdateExternalReferenceIdTransactionItemDTO
+    {
+        [Display(Name = "کلید به جزییات تراکنش")]
+        [Required(ErrorMessage = "کلید به جزییات تراکنش را مشخص کنید")]
+        public long TrsTransactionItemId { get; set; }
+
+        [Display(Name = "کلید مبدا ارجاع")]
+        [Required(ErrorMessage = "کلید مبدا ارجاع را مشخص کنید")]
+        public string ExternalReferenceId { get; set; }
+    }
+}
